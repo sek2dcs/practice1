d00dc2748d8946aedb369c09caf92b2ed48331da
sek2dcs <sek2dcs@virginia.edu>
2025-09-04 10:20:47 -0400

    committing new files

---
+++ get_data.py
@@ -0,0 +1,28 @@
+# %%
+def download_data(force=False):
+    """Download and extract course data from Zenodo."""
+    import urllib.request, zipfile, os
+    
+    zip_path = 'data.zip'
+    data_dir = 'data'
+    
+    if not os.path.exists(zip_path) or force:
+        print("Downloading course data")
+        urllib.request.urlretrieve(
+            'https://zenodo.org/records/16954427/files/data.zip?download=1',
+            zip_path
+        )
+        print("Download complete")
+    else:
+        print("Download file already exists")
+        
+    if not os.path.exists(data_dir) or force:
+        print("Extracting data files...")
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(data_dir)
+        print("Data extracted")
+    else:
+        print("Data directory already exists")
+
+download_data()
+# %%
